72b5ced793d63e7ab05e1590d9285ff881996adc
nox29a <adamczyk29a@interia.pl>
2023-04-26 14:17:45 +0200

    add changes to deploy

--- api/config/env/production/database.js
+++ api/config/env/production/database.js
@@ -1,17 +1,6 @@
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
-      ssl: {
-        ca: env('DATABASE_CA')
-      },
-    },
-    debug: false,
+  url: env("RENDER_EXTERNAL_URL"),
+  dirs: {
+    public: "/data/public"
   },
 });
